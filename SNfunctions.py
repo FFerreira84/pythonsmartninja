da984b332e389e39cbe5f1bb6d73ba8e5eff7b7f
FFerreira84 <frederico.a.ferreira@gmail.com>
2018-05-19 15:19:15 +0100

    Add files via upload

---
+++ SNfunctions.py
@@ -0,0 +1,3 @@
+def func1(num1, num2):
+    x = num1 + num2
+    return x
---
+++ SNfunctiontests.py
@@ -0,0 +1,7 @@
+from SNfunctions import func1
+
+def test_func1():
+    assert func1(4,7) == 11
+    return "Test successful!"
+
+test_func1()
